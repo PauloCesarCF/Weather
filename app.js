d6c564ae8636f97e1cc73d64bc7c5de421862883
PauloCesarCF <pc394108@gmail.com>
2023-01-17 16:58:44 -0300

    Codigo refatorado

--- app.js
+++ app.js
@@ -1,54 +1,65 @@
 const form = document.querySelector('.form-temp');
 const buttonSearchCity = document.querySelector('.search-city');
+const city = document.querySelector('.city');
 
 form.addEventListener('submit', e => {
     e.preventDefault()
 })
 
-const NotFound = () => {
-    return (
-        '<p>Não foi possivel encontrar a cidade citada</p>'
-    )
+const showImageBackground = async () => {
+    const body = document.querySelector('body');
+
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const getNumberRandom = randomNumber(numbers)
+
+    const getImage = await fetch(`https://api.unsplash.com/search/photos?client_id=vxJ9jiqcDmrTxrGN_tQX5jWqLiN2_5S7Or73VjnMtJM&query=${city.value}`)
+    const ImageJson = await getImage.json()
+    const image = ImageJson.results[getNumberRandom].urls.full
+
+    body.style.backgroundImage = `url(${image})`
+    body.style.backgroundPosition = 'center center'
+    body.style.backgroundSize = 'cover'
+
 }
 
 const ShowTemp = (temp) => {
-    const tempo = temp.weather;
-    const description = temp.weather[0].description;
-    const icon = temp.weather[0].icon
 
-
-    const principal = temp.main;
-    const maxima = principal.temp_max;
-    const minima = principal.temp_min;
-    const nuvem = temp.clouds;
-    const pais = temp.sys;
-    const nome = temp.name;
-
-    const flag = `https://countryflagsapi.com/png/${pais.country}`
-
-    const img = document.createElement('img')
-    img.src = flag
+    const responseAPI = {
+        tempNow: temp.weather,
+        description: temp.weather[0].description,
+        icon: temp.weather[0].icon,
+        temp_now: temp.main.temp,
+        temp_max: temp.main.temp_max,
+        temp_min: temp.main.temp_min,
+        clouds: temp.clouds,
+        country: temp.sys,
+        name: temp.name
+    }
 
     return (`
         <div>
             <img src='imgs/localitaion.svg' />
-            <p>${nome} ${principal.temp}°C</p>
+            <p>${responseAPI.name} ${responseAPI.temp_now}°C</p>
         </div>
-        <p>${description} <img src=https://openweathermap.org/img/wn/${icon}.png /></p>
-        <p>Maxima: ${maxima}°C</p>
-        <p>Minima: ${minima}°C</p>
+        <p>${responseAPI.description} <img src=https://openweathermap.org/img/wn/${responseAPI.icon}.png /></p>
+        <p>Maxima: ${responseAPI.temp_max}°C</p>
+        <p>Minima: ${responseAPI.temp_min}°C</p>
     `)
 }
 
 const renderTemp = temp => {
     const section = document.querySelector('section')
 
-    section.innerHTML = temp.cod === '400' | temp.cod === '404' ? NotFound() : ShowTemp(temp)
+    if (temp.cod === '400' | temp.cod === '404') {
+        section.innerHTML = `<p>Não foi possível encontrar a cidade citada`
+        return
+    }
+    section.innerHTML = ShowTemp(temp)
+    showImageBackground()
 };
 
 const getResponseAPI = async () => {
-    const city = document.querySelector('.city');
-    const body = document.querySelector('body');
     const local = city.value;
     const loader = document.querySelector('.containerLoader')
 
@@ -60,24 +71,9 @@ const getResponseAPI = async () => {
         lang: 'pt_br'
     };
 
-    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    const getNumberRandom = randomNumber(numbers)
-
-    const getImage = await fetch(`https://api.unsplash.com/search/photos?client_id=vxJ9jiqcDmrTxrGN_tQX5jWqLiN2_5S7Or73VjnMtJM&query=${city.value}`)
-    const ImageJson = await getImage.json()
-    console.log(ImageJson)
-    const image = ImageJson.results[getNumberRandom].urls.full
-
-    body.style.backgroundImage = `url(${image})`
-    body.style.backgroundPosition = 'center center'
-    body.style.backgroundSize = 'cover'
-
     const get = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${local}&units=${api.units}&lang=${api.lang}&appid=${api.key}`)
     const json = await get.json()
-    console.log(json);
     renderTemp(json);
-
 
     loader.style.display = 'none'
 }
